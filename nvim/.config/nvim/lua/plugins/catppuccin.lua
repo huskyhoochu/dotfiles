65d5435cde0a339a49454c751e4049601a3c60a1
huskyhoochu <dfg1499@gmail.com>
2025-01-21 00:26:35 +0900

    add lazy.lua file

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,4 +2,14 @@ return {
 	"catppuccin/nvim",
 	name = "catppuccin",
 	priority = 1000,
+	opts = {
+		flavour = "mocha",
+		transparent_background = true,
+		integration = {
+			nvimtree = {
+				enable = true,
+				transparent_panel = true,
+			},
+		},
+	},
 }
