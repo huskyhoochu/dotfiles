95df31985d0aeffa47a7e4c5a3afef0ce70f2a3c
huskyhoochu <dfg1499@gmail.com>
2025-06-20 20:13:04 +0900

    add codecompanion setup

---
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -0,0 +1,29 @@
+return {
+	"olimorris/codecompanion.nvim",
+	opts = {
+		strategies = {
+			chat = {
+				adapter = "anthropic",
+			},
+			inline = {
+				adapter = "anthropic",
+			},
+			cmd = {
+				adapter = "anthropic",
+			},
+		},
+		adapters = {
+			anthropic = function()
+				return require("codecompanion.adapters").extend("anthropic", {
+					env = {
+						api_key = os.getenv("ANTHROPIC_API_KEY"),
+					},
+				})
+			end,
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+}
